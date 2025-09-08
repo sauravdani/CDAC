78cedce081737895ce7d55ab59499f951abfd86e
sauravdani <97287044+sauravdani@users.noreply.github.com>
2025-09-08 21:28:55 +0530

    08-09

---
+++ java/CODE/java/src/day1/Calander.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class Calander {
+	public static void main(String[] args) {
+		Date d = new Date();
+		d.setDate(8, "July", 2004);
+		d.display();
+	}
+}
---
+++ java/CODE/java/src/day1/Date.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class Date {
+	private int day;
+	private String month;
+	private int year;
+	
+	public void setDate(int d, String m, int y) {
+		day = d;
+		month = m;
+		year = y;
+	}
+	
+	public void display() {
+		System.out.println(day + "-" + month + "-" + year );
+	}
+	
+}
